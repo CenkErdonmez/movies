e786059ad0a739e598aa8e9bd672c30b3df0e84c
alwaysalty <cenkerdonmez@gmail.com>
2024-01-03 23:10:51 +0300

    css updates

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -43,7 +43,9 @@ function Movies() {
   };
   console.log(movies);
   return (
-    <div className='flex container flex-col  justify-center items-center '>
+    <div
+      className={`flex container flex-col justify-center items-center mx-auto `}
+    >
       {isDeleteModalOpen && selectedMovie && (
         <DeleteModal
           movieId={selectedMovie.id}
@@ -56,35 +58,70 @@ function Movies() {
           movieTitle={selectedMovie.title}
         />
       )}
-      <button
-        className={isDeleteModalOpen || (isUpdateModalOpen && "hidden")}
-        onClick={openAddModalHandler}
-      >
-        Add Product
-      </button>
+      <div className='flex container justify-center w-full items-center gap-4'>
+        <button
+          className={`${
+            isDeleteModalOpen || isUpdateModalOpen ? "hidden" : ""
+          } bg-blue-500 text-white px-4 py-2 rounded-md mb-2 md:mb-0 md:mr-2 my-6 `}
+          onClick={openAddModalHandler}
+        >
+          Add Product
+        </button>
+        <button
+          className={`${
+            isDeleteModalOpen || isUpdateModalOpen ? "hidden" : ""
+          } bg-blue-500 text-white px-4 py-2 rounded-md mb-2 md:mb-0 md:mr-2 my-6 `}
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Reload Data
+        </button>
+      </div>
       {isAddModalOpen && <AddMovieModal />}
 
-      {movies?.movies.products?.map((movie) => (
-        <div
-          className={`${
-            isDeleteModalOpen || isAddModalOpen || isUpdateModalOpen
-              ? "blur "
-              : ""
-          } flex  flex-col gap-6`}
-          key={movie?.id}
-        >
-          <h2 className='bg-red-500 text-green-400'>{movie?.title}</h2>
-          <span>{movie?.price} </span>
-          <img src={movie?.thumbnail} />
-          <button onClick={() => openUpdateModalHandler(movie.id, movie.title)}>
-            Update
-          </button>
-          <button onClick={() => openDeleteModalHandler(movie.id, movie.title)}>
-            Delete
-          </button>
-          <Link to={`/details/${movie.id}`}>Details</Link>
-        </div>
-      ))}
+      <div
+        className={`flex  flex-wrap justify-center items-center py-4 gap-6 mx-auto px-4 container ${
+          isDeleteModalOpen || isAddModalOpen || isUpdateModalOpen
+            ? "blur "
+            : ""
+        } `}
+      >
+        {movies?.movies.products?.map((movie) => (
+          <div
+            className={`flex flex-col gap-4 rounded-md p-4 bg-white shadow-md w-full md:w-80 self-start`}
+            key={movie?.id}
+          >
+            <h2 className='text-lg md:text-xl font-bold'>{movie?.title}</h2>
+            <span className='text-gray-600'>Price: {movie?.price} $</span>
+            <img
+              src={movie?.thumbnail}
+              alt={movie?.title}
+              className='w-full h-40 object-cover mb-2'
+            />
+            <div className='flex flex-col md:flex-row md:justify-between'>
+              <button
+                className='bg-blue-500 text-white px-4 py-2 rounded-md mb-2 md:mb-0 md:mr-2'
+                onClick={() => openUpdateModalHandler(movie.id, movie.title)}
+              >
+                Update
+              </button>
+              <button
+                className='bg-red-500 text-white px-4 py-2 rounded-md'
+                onClick={() => openDeleteModalHandler(movie.id, movie.title)}
+              >
+                Delete
+              </button>
+            </div>
+            <Link
+              to={`/details/${movie.id}`}
+              className='text-blue-500 hover:underline mt-2 self-center'
+            >
+              Details
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
